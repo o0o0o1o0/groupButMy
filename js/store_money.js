d20e1f8e307e4fafb6ff3c35e694320d24495adb
o0o0o1o0 <o0o0o1o0sd@gmail.com>
2020-08-18 09:36:53 +0800

    儲值夜市幣_改為彈窗已完成。

--- js/store_money.js
+++ js/store_money.js
@@ -1,4 +1,20 @@
 function doFirst(){
+    //整個彈跳視窗的顯示
+    $('#clickBoxBtn').click(function(){
+        $('form#storeMoneyForm').css('display','block');
+        $('.container').css('background','rgba(0,0,0,.5)')
+        // $('form#storeMoneyForm .pagewrap').css('max-width','500px');
+    })
+    //關閉按鈕
+    $('.closeBtn').click(function(){
+        $('form#storeMoneyForm').css('display','none');
+        $('.container').css('background','')
+        currentPage=1;
+        $('.pagewrap .page').css('transform','');
+        nextBtn.text("儲值夜市幣");
+        // $('form#storeMoneyForm .pagewrap').css('max-width','0');
+    })
+
     //顯示轉換夜市幣
     $('#ntcoin').keyup(function(){
         let ntdollor =  parseInt($('#ntcoin').val());
@@ -22,10 +38,13 @@ function doFirst(){
     let currentPage = parseInt(mainElm.attr('data-page'));
     let allElm = $('.page').eq(currentPage-1).find('input')
     const nextBtn =  $('#yesNextBtn')
+    currentPage = 1;
     function checkPage(){
-        currentPage = parseInt(mainElm.attr('data-page'));
+        // currentPage = parseInt(mainElm.attr('data-page'));
         if(currentPage==totalPage){
             nextBtn.text("確認儲值");
+        }else if(currentPage==1){
+            nextBtn.text("儲值夜市幣");
         }
     }
     function checkNull(){
@@ -35,24 +54,21 @@ function doFirst(){
             valElm = $(allElm).eq(i).val();
             if(valElm == ""){nullElm+=1};
         }
+
         if(nullElm==0 && currentPage<totalPage){        //未在最後一頁前，填完表格
             currentPage+=1;
-            mainElm.attr('data-page',currentPage);}
+            $('.pagewrap .page').css('transform','translateX(-100%)');
+            // mainElm.attr('data-page',currentPage);       //這是版本一的非燈箱下一頁
+        }
         else if(nullElm==0 && currentPage==totalPage){  //在最後一頁，填完表格
             $('.popbox').css('display','flex');             //🔰這是演示彈跳視窗，正常情況下，此頁面不會有彈跳視窗。
             $('form').css('display','none');                //🔰這是演示彈跳視窗，正常情況下，此頁面不會有彈跳視窗。
             // nextBtn.prop("type", "submit");                 //button改為submit
             // nextBtn.click($('form').submit())              //表單submit送出資料
+            // $('.container').css('background','')           //🔰如果你不要演示彈跳視窗，這個要加上去
         }
         else if(nullElm<=allElm.length){                //未填完表格
             alert('請填寫欄位。');
-            // for(let i=0;i<allElm.length;i++){
-            //     if(valElm == ""){
-            //         allElm[i].className+='error';
-            //     }else if(valElm != ""){
-            //         allElm[i].className-='error';
-            //     }
-            // }
         }
         console.log(`總共${allElm.length}個元素,有${nullElm}個空白`)
     }
@@ -67,9 +83,13 @@ function doFirst(){
         }
     })
 
-    //完成儲值後的彈跳視窗
+    //🔰完成儲值後的彈跳視窗(演示用)
     $("#okIKnow").click(function(){
         $('.popbox').css('display','none');
+        $('.container').css('background','');
+        currentPage=1;
+        $('.pagewrap .page').css('transform','');
+        nextBtn.text("儲值夜市幣");
     })
 }//doFirst
 window.addEventListener('load',doFirst);
